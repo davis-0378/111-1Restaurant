79f2acefe36f4de63351a3b7d143241ec9ace2da
Mojito765 <nicknick765@gmail.com>
2022-12-09 00:55:05 +0800

    3rd commit

--- src/customer/Order.java
+++ src/customer/Order.java
@@ -6,8 +6,10 @@ import java.awt.event.*;
 
 public class Order extends JFrame {
 
-	private JButton button1, button2, button3, button4;
-	private JLabel label1, label2;
+	private static final long serialVersionUID = 936763627297127436L;
+	private JButton button1, button2, button3, button4, button5;
+	private JLabel label1, label2, label3, label4, label5, label6, label7;
+	private JTextField sashimi, sushi, salmon, shrimp;
 
 	public Order() {
 		order();
@@ -16,71 +18,95 @@ public class Order extends JFrame {
 	private void order() {
 		this.setTitle("顧客點餐");
 		this.setLayout(null);
-		this.setResizable(false);
 		this.setSize(500, 500);
+		this.setResizable(false);
 		ImageIcon icon = new ImageIcon();
-		
-		//left
+
+		// top
+		label5 = new JLabel("點擊圖片點餐");
+		label5.setBounds(30, 5, 200, 50);
+		this.add(label5);
+
+		// area1
 		button1 = new JButton("Sashimi $100");
-		button1.setBounds(30, 100, 135, 135);
+		button1.setBounds(30, 55, 135, 135);
 		this.add(button1);
-		button2 = new JButton("Sushi $20");
-		button2.setBounds(180, 100, 135, 135);
+		label1 = new JLabel("$100 Sashimi");
+		label1.setBounds(30, 200, 135, 20);
+		this.add(label1);
+		sashimi = new JTextField("0");
+		sashimi.setHorizontalAlignment(JTextField.CENTER);
+		sashimi.setBounds(130, 200, 35, 20);
+		this.add(sashimi);
+		
+
+		// area2
+		button2 = new JButton("Sushi $50");
+		button2.setBounds(180, 55, 135, 135);
 		this.add(button2);
-		button3 = new JButton("Salmon $90");
+		label2 = new JLabel("$50 Sushi");
+		label2.setBounds(180, 200, 135, 20);
+		this.add(label2);
+		sushi = new JTextField("0");
+		sushi.setHorizontalAlignment(JTextField.CENTER);
+		sushi.setBounds(280, 200, 35, 20);
+		this.add(sushi);
+
+		// area3
+		button3 = new JButton("Salmon $200");
 		button3.setBounds(30, 250, 135, 135);
 		this.add(button3);
-		button4 = new JButton("Shrimp $50");
+		label3 = new JLabel("$200 Salmon");
+		label3.setBounds(30, 395, 135, 20);
+		this.add(label3);
+		salmon = new JTextField("0");
+		salmon.setHorizontalAlignment(JTextField.CENTER);
+		salmon.setBounds(130, 395, 35, 20);
+		this.add(salmon);
+		
+
+		// area4
+		button4 = new JButton("Shrimp $100");
 		button4.setBounds(180, 250, 135, 135);
 		this.add(button4);
+		label4 = new JLabel("$100 Shrimp");
+		label4.setBounds(180, 395, 135, 20);
+		this.add(label4);
+		shrimp = new JTextField("0");
+		shrimp.setHorizontalAlignment(JTextField.CENTER);
+		shrimp.setBounds(280, 395, 35, 20);
+		this.add(shrimp);
 
 		// right
-		label1 = new JLabel("<-點擊圖片點餐");
-		label1.setBounds(350, 100, 200, 50);
-		this.add(label1);
-		
-		final List list = new List();
-		list.setBounds(350, 150, 100, 180);
-		this.add(list);
+		label6 = new JLabel("總金額：");
+		label6.setBounds(350, 330, 200, 25);
+		this.add(label6);
 
-		label2 = new JLabel("總金額：0元");
-		label2.setBounds(350, 330, 200, 50);
-		this.add(label2);
+		button5 = new JButton("重新點餐");
+		button5.setBounds(350, 360, 100, 25);
+		this.add(button5);
+		label7 = new JLabel("0");
+		this.add(label7);
+		label7.setBounds(410, 330, 100, 25);
 
 		// btn1
 		icon = new ImageIcon("src/res/pexels-cup-of-couple-8472651.jpg");
 		button1.setIcon(icon);
-		
+
 		button1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				String[] items = list.getItems();
-				boolean flag = false;
-				int index = 0;
-				for (int i = 0; i < items.length; i++) {
-					if (items[i].contains("Sashimi")) {
-						index = i;
-						flag = true;
-						break;
-					}
-				}
-				if (flag) {
-					String item = list.getItem(index);
-					int x_index = item.indexOf("*");
-					String value = item.substring(x_index + 1);
-					item = item.substring(0, x_index + 1) + (Integer.parseInt(value) + 1);
-					list.remove(index);
-					list.add(item, index);
-				} else {
-					list.add("Sashimi *1");
-				}
-				String value = label2.getText();
-				int begin = value.indexOf("：");
-				int end = value.indexOf("元");
-				String money = value.substring(begin + 1, end);
-				String newValue = String.valueOf((Integer.parseInt(money) + 100));
-				value = value.replaceAll(money, newValue);
-				label2.setText(value);
+				String total1 = label7.getText();
+				int n = Integer.parseInt(total1);
+				n += 100;
+				String total2 = String.valueOf(n);
+				label7.setText(total2);
+				
+				String num1 = sashimi.getText();
+				int x =Integer.parseInt(num1);
+				x += 1 ;
+				String num2 = String.valueOf(x);
+				sashimi.setText(num2);
 			}
 		});
 
@@ -91,33 +117,17 @@ public class Order extends JFrame {
 		button2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				String[] items = list.getItems();
-				boolean flag = false;
-				int index = 0;
-				for (int i = 0; i < items.length; i++) {
-					if (items[i].contains("Sushi")) {
-						index = i;
-						flag = true;
-						break;
-					}
-				}
-				if (flag) {
-					String item = list.getItem(index);
-					int x_index = item.indexOf("*");
-					String value = item.substring(x_index + 1);
-					item = item.substring(0, x_index + 1) + (Integer.parseInt(value) + 1);
-					list.remove(index);
-					list.add(item, index);
-				} else {
-					list.add("Sushi *1");
-				}
-				String value = label2.getText();
-				int begin = value.indexOf("：");
-				int end = value.indexOf("元");
-				String money = value.substring(begin + 1, end);
-				String newValue = String.valueOf((Integer.parseInt(money) + 20));
-				value = value.replaceAll(money, newValue);
-				label2.setText(value);
+				String total1 = label7.getText();
+				int n = Integer.parseInt(total1);
+				n += 50;
+				String total2 = String.valueOf(n);
+				label7.setText(total2);
+				
+				String num1 = sushi.getText();
+				int x =Integer.parseInt(num1);
+				x += 1 ;
+				String num2 = String.valueOf(x);
+				sushi.setText(num2);
 			}
 
 		});
@@ -129,33 +139,17 @@ public class Order extends JFrame {
 		button3.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				String[] items = list.getItems();
-				boolean flag = false;
-				int index = 0;
-				for (int i = 0; i < items.length; i++) {
-					if (items[i].contains("Salmon")) {
-						index = i;
-						flag = true;
-						break;
-					}
-				}
-				if (flag) {
-					String item = list.getItem(index);
-					int x_index = item.indexOf("*");
-					String value = item.substring(x_index + 1);
-					item = item.substring(0, x_index + 1) + (Integer.parseInt(value) + 1);
-					list.remove(index);
-					list.add(item, index);
-				} else {
-					list.add("Salmon *1");
-				}
-				String value = label2.getText();
-				int begin = value.indexOf("：");
-				int end = value.indexOf("元");
-				String money = value.substring(begin + 1, end);
-				String newValue = String.valueOf((Integer.parseInt(money) + 90));
-				value = value.replaceAll(money, newValue);
-				label2.setText(value);
+				String total1 = label7.getText();
+				int n = Integer.parseInt(total1);
+				n += 200;
+				String total2 = String.valueOf(n);
+				label7.setText(total2);
+				
+				String num1 = salmon.getText();
+				int x =Integer.parseInt(num1);
+				x += 1 ;
+				String num2 = String.valueOf(x);
+				salmon.setText(num2);
 			}
 
 		});
@@ -167,48 +161,39 @@ public class Order extends JFrame {
 		button4.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				String[] items = list.getItems();
-				boolean flag = false;
-				int index = 0;
-				for (int i = 0; i < items.length; i++) {
-					if (items[i].contains("Shrimp")) {
-						index = i;
-						flag = true;
-						break;
-					}
-				}
-				if (flag) {
-					String item = list.getItem(index);
-					int x_index = item.indexOf("*");
-					String value = item.substring(x_index + 1);
-					item = item.substring(0, x_index + 1) + (Integer.parseInt(value) + 1);
-					list.remove(index);
-					list.add(item, index);
-				} else {
-					list.add("Shrimp *1");
-				}
-				String value = label2.getText();
-				int begin = value.indexOf("：");
-				int end = value.indexOf("元");
-				String money = value.substring(begin + 1, end);
-				String newValue = String.valueOf((Integer.parseInt(money) + 50));
-				value = value.replaceAll(money, newValue);
-				label2.setText(value);
+				String total1 = label7.getText();
+				int n = Integer.parseInt(total1);
+				n += 100;
+				String total2 = String.valueOf(n);
+				label7.setText(total2);
+				
+				String num1 = shrimp.getText();
+				int x =Integer.parseInt(num1);
+				x += 1 ;
+				String num2 = String.valueOf(x);
+				shrimp.setText(num2);
 			}
 		});
-		
+
+		// btn5
+		button5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sashimi.setText(String.valueOf(0));
+				sushi.setText(String.valueOf(0));
+				salmon.setText(String.valueOf(0));
+				shrimp.setText(String.valueOf(0));
+				label7.setText(String.valueOf(0));
+			}
+		});
+
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
 
 /*
- * btn1
- * 攝影師：Cup of Couple: https://www.pexels.com/zh-tw/photo/8472651/
- * btn2
- * 攝影師：Luiz Fernando Maciel: https://www.pexels.com/zh-tw/photo/7717517/
- * btn3
- * 攝影師：Horizon Content: https://www.pexels.com/zh-tw/photo/3763644/
- * btn4
+ * btn1 攝影師：Cup of Couple: https://www.pexels.com/zh-tw/photo/8472651/ btn2
+ * 攝影師：Luiz Fernando Maciel: https://www.pexels.com/zh-tw/photo/7717517/ btn3
+ * 攝影師：Horizon Content: https://www.pexels.com/zh-tw/photo/3763644/ btn4
  * 攝影師：cottonbro studio: https://www.pexels.com/zh-tw/photo/3295855/
  */
--- src/login/Login.java
+++ src/login/Login.java
@@ -6,10 +6,11 @@ import java.awt.event.*;
 
 public class Login extends JFrame {
 
+	private static final long serialVersionUID = -4352093231107430236L;
 	private JPanel jpanel1, jpanel2, jpanel3, jpanel4, jpanel5;
+	private JLabel label1, label2, label3;
 	private JTextField username;
 	private JPasswordField password;
-	private JLabel label1, label2, label3;
 	private JButton button1;
 	
 	public Login() {
@@ -18,15 +19,21 @@ public class Login extends JFrame {
 
 	private void login() {
 		this.setTitle("Login Page");
+		this.setLayout(new GridLayout(5, 1));
+		this.setSize(500, 500);
+		this.setResizable(false);
 		
+		// jpanel1
 		jpanel1 = new JPanel();
 		this.add(jpanel1);
 		
+		// jpanel2
 		jpanel2 = new JPanel();
 		this.add(jpanel2);
 		label1 = new JLabel("Welcome to Login Page!");
 		jpanel2.add(label1);
 		
+		// jpanel3
 		jpanel3 = new JPanel();
 		this.add(jpanel3);
 		label2 = new JLabel("Username:");
@@ -34,6 +41,7 @@ public class Login extends JFrame {
 		username = new JTextField(10);
 		jpanel3.add(username);
 		
+		// jpanel4
 		jpanel4 = new JPanel();
 		this.add(jpanel4);
 		label3 = new JLabel("Password:");
@@ -41,16 +49,15 @@ public class Login extends JFrame {
 		password = new JPasswordField(10);
 		jpanel4.add(password);
 		
+		// jpanel5
 		jpanel5 = new JPanel();
 		this.add(jpanel5);
 		button1 = new JButton("Log in");
 		jpanel5.add(button1);
 		
-		this.setLayout(new GridLayout(5, 10));
-		this.setSize(500, 500);
-		this.setResizable(false);
 		
-		// btn1_login
+		
+		// btn1
 		button1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -60,7 +67,7 @@ public class Login extends JFrame {
 				if (username.getText().length() == 0 || new String(password.getPassword()).length() == 0)
 					JOptionPane.showMessageDialog(null, "Username and password cannot be empty.", "Oops!",JOptionPane.WARNING_MESSAGE);
 				else if (username.getText().equals("廚師") && new String(password.getPassword()).equals("000")) {
-					result = JOptionPane.showConfirmDialog(null, "Welcome 廚師! Work now?", "", JOptionPane.YES_NO_OPTION);
+					result = JOptionPane.showConfirmDialog(null, "Hi 廚師! Work now?", "Welcome", JOptionPane.YES_NO_OPTION);
 					if (result == 0) {
 						dispose();
 						new Cook();
@@ -70,7 +77,7 @@ public class Login extends JFrame {
 					}
 				}
 				else if (username.getText().equals("服務生") && new String(password.getPassword()).equals("111")) {
-					result = JOptionPane.showConfirmDialog(null, "Welcome 服務生! Work now?", "", JOptionPane.YES_NO_OPTION);
+					result = JOptionPane.showConfirmDialog(null, "Hi 服務生! Work now?", "Welcome", JOptionPane.YES_NO_OPTION);
 					if (result == 0) {
 						dispose();
 						new Server();
@@ -80,7 +87,7 @@ public class Login extends JFrame {
 					}
 				}
 				else if (username.getText().equals("櫃檯") && new String(password.getPassword()).equals("222")) {
-					result = JOptionPane.showConfirmDialog(null, "Welcome 櫃檯! Work now?", "", JOptionPane.YES_NO_OPTION);
+					result = JOptionPane.showConfirmDialog(null, "Hi 櫃檯! Work now?", "Welcome", JOptionPane.YES_NO_OPTION);
 					if (result == 0) {
 						dispose();
 						new Reception();
@@ -90,7 +97,7 @@ public class Login extends JFrame {
 					}
 				}
 				else if (username.getText().equals("雜工") && new String(password.getPassword()).equals("333")) {
-					result = JOptionPane.showConfirmDialog(null, "Welcome 雜工! Work now?", "", JOptionPane.YES_NO_OPTION);
+					result = JOptionPane.showConfirmDialog(null, "Hi 雜工! Work now?", "Welcome", JOptionPane.YES_NO_OPTION);
 					if (result == 0) {
 						dispose();
 						new Manager();
@@ -100,7 +107,7 @@ public class Login extends JFrame {
 					}
 				}
 				else if (username.getText().equals("經理") && new String(password.getPassword()).equals("444")) {
-					result = JOptionPane.showConfirmDialog(null, "Welcome 經理! Work now?", "", JOptionPane.YES_NO_OPTION);
+					result = JOptionPane.showConfirmDialog(null, "Hi 經理! Work now?", "Welcome", JOptionPane.YES_NO_OPTION);
 					if (result == 0) {
 						dispose();
 						new Others();
